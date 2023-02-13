f11db2ff948855419795d54b393d152216eaf0eb
Nicolas AUBIN <aubinnic@gmail.com>
2023-02-13 11:52:12 +0100

    Update merger and schema files

--- global_config_generator.py
+++ global_config_generator.py
@@ -4,7 +4,7 @@ import urllib
 import requests
 
 from collections import OrderedDict
-from jsonmerge import merge
+from jsonmerge import Merger
 from global_config_logger import Logger
 
 
@@ -21,6 +21,8 @@ class GlobalConfigGenerator:
         """
         self.config_in_path = config_in_path
         self.logger = Logger(logger)
+
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
 
         self.config_file = os.path.join(
             self.config_in_path, 'globalConfig.json'
@@ -52,12 +54,15 @@ class GlobalConfigGenerator:
                 # parse config JSON with original order of keys
                 config = json.load(f, object_pairs_hook=OrderedDict)
 
+            if self.is_json(config):
+                return config
+            else:
+                return
+
         except Exception as e:
             msg = "Error loading GlobalConfigGenerator config:\n%s" % e
             self.logger.error(msg)
             return
-
-        return config
 
     def read_index_html(self):
         if not os.path.exists(self.index_file):
@@ -80,7 +85,9 @@ class GlobalConfigGenerator:
         tenant_path = os.path.join(self.config_in_path, tenant)
         tenant_output_file = os.path.join(tenant_path, outputfile)
         # Merge two json files
-        merged_config = merge(common_config, specific_config, schema)
+        merger = Merger(schema)
+        merged_config = merger.merge(common_config, specific_config)
+        self.logger.debug(merger.get_schema())
         # Creates dirs if not exists
         os.makedirs(tenant_path, exist_ok=True)
         # Write config
@@ -105,38 +112,47 @@ class GlobalConfigGenerator:
             self.logger.error(msg)
 
     def write_configs(self):
-        specific_configs = self.config.get('specific_configs', [])
-        common_config = self.config.get('common_config', {})
-        for specific_config in specific_configs:
-            tenant = specific_config.get('tenant', '')
-            if tenant:
-                common_cfggensrv_data = common_config.get('config-generator-service', {})
-                specific_cfggensrv_data = specific_config.get('config-generator-service', {})
-                cfggensrv_schema = self.merge_schema_for_config_generator()
-                # Add tenant info
-                specific_cfggensrv_data['config'] = specific_cfggensrv_data.get('config', {})
-                specific_cfggensrv_data['config'].update(tenant=tenant)
-                #Create qwc-config-generator file : tenantConfig.json
-                self.create_tenant_config_file(tenant,
-                                               common_cfggensrv_data,
-                                               specific_cfggensrv_data,
-                                               'tenantConfig.json',
-                                               cfggensrv_schema)
+        if self.config and self.html:
+            specific_configs = self.config.get('specific_configs', [])
+            common_config = self.config.get('common_config', {})
+            merge_schema_cfggensrv = self.merge_schema_for_config_generator()
+            merge_schema_qwc2cfg = self.merge_schema_for_qwc2_config()
+            for specific_config in specific_configs:
+                tenant = specific_config.get('tenant', '')
+                if tenant:
+                    common_cfggensrv_data = common_config.get('config-generator-service', {})
+                    specific_cfggensrv_data = specific_config.get('config-generator-service', {})
+                    # Add tenant info
+                    specific_cfggensrv_data['config'] = specific_cfggensrv_data.get('config', {})
+                    specific_cfggensrv_data['config'].update(tenant=tenant)
+                    #Create qwc-config-generator file : tenantConfig.json
+                    self.create_tenant_config_file(tenant,
+                                                   common_cfggensrv_data,
+                                                   specific_cfggensrv_data,
+                                                   'tenantConfig.json',
+                                                   merge_schema_cfggensrv)
 
-                common_qwc2config_data = common_config.get('qwc2config', {})
-                specific_qwc2config_data = specific_config.get('qwc2config', {})
-                # Create qwc2 file : config.json
-                self.create_tenant_config_file(tenant, common_qwc2config_data, specific_qwc2config_data, 'config.json')
+                    common_qwc2config_data = common_config.get('qwc2config', {})
+                    specific_qwc2config_data = specific_config.get('qwc2config', {})
+                    # Create qwc2 file : config.json
+                    self.create_tenant_config_file(tenant,
+                                                   common_qwc2config_data,
+                                                   specific_qwc2config_data,
+                                                   'config.json',
+                                                   merge_schema_qwc2cfg)
 
-                # Create qwc2 file : index.html
-                self.create_tenant_index_file(tenant)
+                    # Create qwc2 file : index.html
+                    self.create_tenant_index_file(tenant)
 
-                # Generate configs and permissions with qwc-config-generator-service
-                self.generate_tenant_config(tenant)
+                    # Generate configs and permissions with qwc-config-generator-service
+                    self.generate_tenant_config(tenant)
 
-            else:
-                msg = "'tenant' parameter missing or empty. 'tenant' parameter should be present in each specific config."
-                self.logger.error(msg)
+                else:
+                    msg = "'tenant' parameter missing or empty. 'tenant' parameter should be present in each specific config."
+                    self.logger.error(msg)
+        else:
+            msg = "globalConfig.json or index.html not defined."
+            self.logger.error(msg)
 
     def generate_tenant_config(self, tenant):
         try:
@@ -157,14 +173,53 @@ class GlobalConfigGenerator:
             self.logger.error(msg)
 
     def merge_schema_for_config_generator(self):
-        schema = {
-            "properties": {
-                "services": {
-                    "mergeStrategy": "arrayMergeById",
-                    "mergeOptions": {"idRef": "name"}
-                }
-            }
-        }
+        schema_file = os.path.join(self.root_dir, 'merge_schema_for_config_generator.json')
+        try:
+            with open(schema_file, encoding='utf-8') as f:
+                # parse config JSON with original order of keys
+                schema = json.load(f, object_pairs_hook=OrderedDict)
+
+                if self.is_json(schema):
+                    return schema
+                else:
+                    msg = "Unable to read merge schema for config-generator config"
+                    self.logger.error(msg)
+                    return {}
+
+        except Exception as e:
+            msg = "Error loading merge schema for config-generator config:\n%s" % e
+            self.logger.error(msg)
+            return {}
 
         return schema
+
+    def merge_schema_for_qwc2_config(self):
+        schema_file = os.path.join(self.root_dir, 'merge_schema_for_qwc2_config.json')
+        try:
+            with open(schema_file, encoding='utf-8') as f:
+                # parse config JSON with original order of keys
+                schema = json.load(f, object_pairs_hook=OrderedDict)
+
+                if self.is_json(schema):
+                    return schema
+                else:
+                    msg = "Unable to read merge schema for qwc2 config"
+                    self.logger.error(msg)
+                    return {}
+
+        except Exception as e:
+            msg = "Error loading merge schema for qwc2 config:\n%s" % e
+            self.logger.error(msg)
+            return {}
+
+        return schema
+
+    def is_json(self, json_data):
+        try:
+            json.loads(json.dumps(json_data))
+        except ValueError as e:
+            msg = "Error reading json data:\n%s" % str(e)
+            self.logger.error(msg)
+            return False
+        return True
 
